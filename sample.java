c8bef0dbedc32fc4a6827ce44a7973df10b932bd
uday kumar <udaykumar.gorrepati123@gmail.com>
2021-01-06 12:13:07 +0530

    addition

--- sample.java
+++ sample.java
@@ -1,7 +1,11 @@
 class uday
 {
     public static void main(String[] args) {
+        int a=10;
+        int b=20;
         System.out.println("uday");
         System.out.println("ajay kumar");
+        System.out.print("additon of a nad b is:");
+        System.out.println(a+b);
     }
 }
