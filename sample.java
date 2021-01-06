84e6f3aff8a5b936f45669439a3e2cc391c90d32
uday kumar <udaykumar.gorrepati123@gmail.com>
2021-01-06 12:21:22 +0530

    reverse number

--- sample.java
+++ sample.java
@@ -7,5 +7,15 @@ class uday
         System.out.println("ajay kumar");
         System.out.print("additon of a nad b is:");
         System.out.println(a+b);
+        int num = 167;
+        int rev=0;
+        int rem = 0;
+        while(num>0)
+        {
+            rem  = num%10;
+            rev = rev*10+rem;
+            num=num/10;
+        }
+        System.out.println("reversed number is: "+rev);
     }
 }
